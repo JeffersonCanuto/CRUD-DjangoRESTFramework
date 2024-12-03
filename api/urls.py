441e2ef680d2eabfee36a81582c40d9e6da550e5
jefferson <jeffersondscanuto@gmail.com>
2024-12-03 08:47:37 -0400

    feat: create get/read method in views and set it as a route in urls file

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.ApiOverview, name="api-overview")
+    path('', views.ApiOverview, name='api-overview'),
+    path('task-read/<str:pk>/', views.TaskRead, name='task-read')
 ]
--- api/views.py
+++ api/views.py
@@ -4,15 +4,28 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .models import Task
+from .serializers import TaskSerializer
+
 # Create your views here.
 @api_view(["GET"])
 def ApiOverview(request):
-    api_urls = {
-        "List": "/task-list/",
-        "Detail View": "/task-detail/<str:pk>/",
+    return Response({
         "Create": "/task-create/",
+        "Read": "/task-read/",
         "Update": "/task-update/<str:pk>/",
         "Delete": "/task-delete/<str:pk>/"
-    }
+    })
 
-    return Response(api_urls)
+@api_view(["GET"])
+def TaskRead(request, pk):
+    try:
+        if pk == 'all':
+            tasks = Task.objects.all()
+        else:
+            tasks = Task.objects.all().filter(id=pk)
+
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+    except Exception as error:
+        print(error)
